81fb6417433e7eb06a9960e6072fa933a5e9d2b7
mayrez <mayrezbritto@gmail.com>
2016-05-02 15:34:29 +0100

    template

--- template.rb
+++ template.rb
@@ -1,7 +1,67 @@
-generate(:scaffold, "person name:string")
-route "root to: 'people#index'"
+generate(:scaffold, "comentario titulo:string cuerpo:string")
+route "root to: 'comentarios#index'"
 
-file 'config/database.yml', <<-CODE
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.1.6'
+# Use postgresql as the database for Active Record
+gem 'pg'
+# Use SCSS for stylesheets
+gem 'sass-rails', '~> 4.0.3'
+# Use Uglifier as compressor for JavaScript assets
+gem 'uglifier', '>= 1.3.0'
+# Use CoffeeScript for .coffee assets and views
+gem 'coffee-rails', '~> 4.0.0'
+# See https://github.com/sstephenson/execjs#readme for more supported runtimes
+# gem 'therubyracer', platforms: :ruby
+
+# Use jquery as the JavaScript library
+gem 'jquery-rails'
+# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
+gem 'turbolinks'
+# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+gem 'jbuilder', '~> 2.0'
+# bundle exec rake doc:rails generates the API under doc/api.
+gem 'sdoc', '~> 0.4.0', group: :doc
+
+gem_group :test do
+  gem 'capybara'
+  # Poltergeist is a driver for Capybara.
+  # It allows you to run your Capybara tests on a headless WebKit...
+  gem 'poltergeist'
+  gem 'phantomjs', :require => 'phantomjs/poltergeist'
+
+end
+
+gem_group :development, :test do
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug'
+
+  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'spring'
+
+  # rspec-rails is a testing framework for Rails 3.x and 4.x
+  gem 'rspec-rails', '~> 3.0'
+end
+
+gem_group :development do
+  # Access an IRB console on exception pages or by using <%= console %> in views
+  gem 'web-console'
+  gem 'travis'
+end
+gem 'bootstrap-sass', '~> 3.0'
+
+
+gem_group :production do
+  gem 'rails_12factor'
+end
+gem 'carrierwave'
+gem 'puma'
+gem 'mini_magick'
+gem "fog"
+#gem 'carrierwave_direct', :git => "git://github.com/dwilkie/carrierwave_direct.git"
+run "bundle install"
+
+file 'config/database.yml.postgres', <<-CODE
 # PostgreSQL. Versions 8.2 and up are supported.
 #
 # Install the pg driver:
@@ -95,70 +155,12 @@ production:
   password: <%= ENV['SHOEADDICT_APP_DATABASE_PASSWORD'] %>
 
 CODE
-
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.1.6'
-# Use postgresql as the database for Active Record
-gem 'pg'
-# Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
-# Use Uglifier as compressor for JavaScript assets
-gem 'uglifier', '>= 1.3.0'
-# Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.1.0'
-# See https://github.com/sstephenson/execjs#readme for more supported runtimes
-# gem 'therubyracer', platforms: :ruby
-
-# Use jquery as the JavaScript library
-gem 'jquery-rails'
-# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.0'
-# bundle exec rake doc:rails generates the API under doc/api.
-gem 'sdoc', '~> 0.4.0', group: :doc
-
-gem_group :test do
-  gem 'capybara'
-  # Poltergeist is a driver for Capybara.
-  # It allows you to run your Capybara tests on a headless WebKit...
-  gem 'poltergeist'
-  gem 'phantomjs', :require => 'phantomjs/poltergeist'
-
-end
-
-gem_group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
-
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'spring'
-
-  # rspec-rails is a testing framework for Rails 3.x and 4.x
-  gem 'rspec-rails', '~> 3.0'
-end
-
-gem_group :development do
-  # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console'
-  gem 'travis'
-end
-gem 'bootstrap-sass', '~> 3.0'
-
-
-gem_group :production do
-  gem 'rails_12factor'
-end
-
-gem 'puma'
-gem 'mini_magick'
-gem "fog"
-gem 'carrierwave_direct', :git => "git://github.com/dwilkie/carrierwave_direct.git"
-
-# Final install steps
-rake('gems:install', :sudo => true)
-rake('db:sessions:create')
+run "cp config/database.yml.postgres config/database.yml" 
+rake('db:create')
 rake('db:migrate')
+# Final install steps
+#rake('gems:install')
+
 git :init
 git add: "."
 git commit: %Q{ -m 'Initial commit' }
